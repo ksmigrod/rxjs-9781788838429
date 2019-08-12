60214c8f50bbd34309adf3961b783182ffd5258b
Krzysztof Śmigrodzki <Krzysztof.Smigrodzki@mf.gov.pl>
2019-08-12 17:58:08 +0200

    An example for interval.

--- main.ts
+++ main.ts
@@ -1,32 +1,14 @@
 import './css/main.css';
-import {Observable} from 'rxjs';
+import {interval, Observable} from 'rxjs';
 
-// ****
-//
-// Welcome to your rxjs scratch pad 🤗
-// 
-// ****
+const source$ = interval(500);
 
-//
-// const source$: Observable<number> = Observable.create((observer) => {
-//     let i = 1;
-//     const id = setInterval(() => {
-//         observer.next(i++);
-//     }, 1000);
-// });
-
-const source$ = new Observable<number>((observer) => {
-    let i = 1;
-    setInterval(() => {
-        observer.next(i++);
-        if (i == 20) {
-            observer.complete();
-        }
-    }, 1000);
-});
-
-source$.subscribe(
+let subscription = source$.subscribe(
     console.log,
     console.warn,
     () => console.log('complete')
 );
+
+setTimeout(() => {
+    subscription.unsubscribe()
+}, 4002);
