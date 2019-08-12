2574a37d839d7e625dcfd89b42ecbf100fe81aca
Krzysztof Śmigrodzki <Krzysztof.Smigrodzki@mf.gov.pl>
2019-08-12 18:01:07 +0200

    An example for range.

--- main.ts
+++ main.ts
@@ -1,29 +1,7 @@
 import './css/main.css';
-import {Observable} from 'rxjs';
+import {range} from "rxjs";
 
-// ****
-//
-// Welcome to your rxjs scratch pad 🤗
-// 
-// ****
-
-//
-// const source$: Observable<number> = Observable.create((observer) => {
-//     let i = 1;
-//     const id = setInterval(() => {
-//         observer.next(i++);
-//     }, 1000);
-// });
-
-const source$ = new Observable<number>((observer) => {
-    let i = 1;
-    setInterval(() => {
-        observer.next(i++);
-        if (i == 20) {
-            observer.complete();
-        }
-    }, 1000);
-});
+const source$ = range(0, 20);
 
 source$.subscribe(
     console.log,
