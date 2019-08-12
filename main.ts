423db8615dd274fe6abbd00fa7c1757ae9b29ef7
Krzysztof Śmigrodzki <Krzysztof.Smigrodzki@mf.gov.pl>
2019-08-12 16:33:48 +0200

    An example for throwError.

--- main.ts
+++ main.ts
@@ -1,31 +1,9 @@
 import './css/main.css';
-import {Observable} from 'rxjs';
+import {defer, from, Observable, throwError} from 'rxjs';
 
-// ****
-//
-// Welcome to your rxjs scratch pad 🤗
-// 
-// ****
+const errorSource$ = throwError('error message');
 
-//
-// const source$: Observable<number> = Observable.create((observer) => {
-//     let i = 1;
-//     const id = setInterval(() => {
-//         observer.next(i++);
-//     }, 1000);
-// });
-
-const source$ = new Observable<number>((observer) => {
-    let i = 1;
-    setInterval(() => {
-        observer.next(i++);
-        if (i == 20) {
-            observer.complete();
-        }
-    }, 1000);
-});
-
-source$.subscribe(
+errorSource$.subscribe(
     console.log,
     console.warn,
     () => console.log('complete')
